664c5d908020c8aaee5b0f358d720c2446d02326
Justin L Sanders <justin@pointw.com>
2020-01-14 20:59:17 -0600

    Update Pygex to NFS Machines

--- pygex/engine.py
+++ pygex/engine.py
@@ -1,25 +1,63 @@
 #!/usr/bin/env python
 
 from contextlib import suppress
+from itertools import tee
+
+class WildCardTransition:
+    def valid_transition_character(self, character):
+        return True
+
+class SingleCharacterTransition:
+    def __init__(self, character):
+        self.character = character
+
+    def valid_transition_character(self, character):
+        return self.character == character
+
+class State:
+    @classmethod
+    def end_state(cls):
+        state = cls()
+        state._end_state = True
+        return state
+
+    def __init__(self):
+        self.transitions = []
+        self._end_state = False
+
+    def add_state_transition(self, character, transition_state):
+        if character == '.':
+            transition = WildCardTransition()
+        else:
+            transition = SingleCharacterTransition(character)
+
+        self.transitions.append((transition, transition_state))
+
+    def is_end_state(self):
+        return self._end_state
+
 
 def matches(string, pattern):
-    parts = _compile(pattern)
-    string_counter = 0
+    start_state = compile(pattern)
+    return state_match(string, start_state)
 
-    for part in parts:
-        if part.endswith('*'):
-            char = part[0]
-            while string_counter < len(string) and string[string_counter] == char:
-                string_counter += 1
-        else:
-            if string_counter >= len(string) or (part != string[string_counter] and part != '.'):
-                return False
-            string_counter += 1
-    return True
+def state_match(string, start_state):
+    if start_state.is_end_state():
+        return True
+
+    try:
+        head, *tail = string
+    except ValueError:
+        return False
+
+    for transition, state in start_state.transitions: #t ca*tt
+        if transition.valid_transition_character(head) and state_match(tail, state):
+            return True
+
+    return False
 
 
-
-def _compile(pattern):
+def break_parts(pattern):
     parts = []
 
     pattern_iter = iter(pattern)
@@ -36,5 +74,49 @@ def _compile(pattern):
             if last_part != "*" and last_part is not None:
                 parts.append(last_part)
             break
-
     return parts
+
+
+def compile(pattern): #.*c
+    start_state = State()
+    parts = break_parts(pattern)
+    states = [start_state]
+    for _ in range(len(parts)):
+        states.append(State())
+    #end_state = State.end_state()
+    #states.append(end_state)
+
+
+    parts_iter = iter(parts)
+    states_iter = iter(states)
+
+    for state in states[:-1]:
+        parts_iter, next_parts = tee(parts_iter)
+
+        next(states_iter)
+        states_iter, next_states = tee(states_iter)
+
+        add_transitions_to_state(state, next_parts, next_states)
+        next(parts_iter)
+
+    states[-1]._end_state = True
+
+    return start_state
+
+
+def add_transitions_to_state(state, parts, next_states):
+    part = next(parts)#T
+    next_state = next(next_states)#4
+    with suppress(StopIteration):
+        while part.endswith('*'):
+            char = part[0]
+
+            state.add_state_transition(char, next_state)
+            next_state.add_state_transition(char, next_state)
+
+            next_state = next(next_states)
+            part = next(parts)
+
+
+    state.add_state_transition(part, next_state)
+
--- tests/test_pygex.py
+++ tests/test_pygex.py
@@ -15,9 +15,13 @@ def test_literal_match_failure_different_case():
 def test_pattern_with_wildcard():
     assert pygex.matches('cat', 'c.t')
     assert pygex.matches('cat', '.at')
+    assert pygex.matches('cat', 'ca.')
 
 def test_pattern_with_asteriks_matches_none():
     assert pygex.matches('ct', 'ca*t')
+
+def test_pattern_with_asteriks_and_extra_charachter_doesnt_match():
+    assert not pygex.matches('ct', 'ca*tt')
 
 def test_patterns_with_asteriks_matches_one():
     assert pygex.matches('cat', 'ca*t')
