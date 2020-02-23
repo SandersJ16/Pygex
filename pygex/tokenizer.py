4619dd0c4bfea3b0d1c4de316547b03df65443c8
Justin L Sanders <justin@pointw.com>
2020-02-23 17:37:29 -0600

    Add base test for repetition tokens

--- pygex/tokenizer.py
+++ pygex/tokenizer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
+special_character = ["^", ".", "[", "]", "$", "(", ")", "\\", "*", "{", "}", "?", "+", "|"]
+repetition_tokens = ["*", "?", "+"]
+
 def tokenize(pattern):
     tokens = []
     for part in pattern:
-        tokens.append(part)
+        if part in repetition_tokens:
+            tokens[-1] += part
+        else:
+            tokens.append(part)
 
     return tokens
 
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -9,3 +9,7 @@ def test_tokenizer_empty_string():
 def test_tokenizer_no_special_characters():
     assert pygex.tokenize("abc") == ["a", "b", "c"]
 
+def test_tokenizer_character_with_single_modifier():
+    assert pygex.tokenize("a*") == ["a*"]
+    assert pygex.tokenize("b+") == ["b+"]
+    assert pygex.tokenize("c?") == ["c?"]
