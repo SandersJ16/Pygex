e17be9c6f2760b6af37e9b18c34d698863f6d8a9
Justin L Sanders <justin@pointw.com>
2020-02-23 17:08:21 -0600

    Add empty string test

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -3,6 +3,9 @@
 import pytest
 from .. import pygex
 
+def test_tokenizer_empty_string():
+    assert pygex.tokenize("") == []
+
 def test_tokenizer_no_special_characters():
     assert pygex.tokenize("abc") == ["a", "b", "c"]
 
