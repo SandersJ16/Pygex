ebfcdb9a295ee427e8156ad261e319fc0eb79257
Justin L Sanders <sandersj1661@gmail.com>
2022-06-05 22:51:18 -0500

    Start testing Tokenizer

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -3,8 +3,9 @@
 import pytest
 from .. import pygex
 
-def regex_repetition_tokens():
-    return ["+", "*", "?"]
+@pytest.fixture(params=["+", "*", "?"])
+def regex_repetition_token(request):
+    return request.param
 
 def test_tokenizer_empty_string():
     assert pygex.tokenize("") == []
@@ -12,11 +13,9 @@ def test_tokenizer_empty_string():
 def test_tokenizer_no_special_characters():
     assert pygex.tokenize("abc") == ["a", "b", "c"]
 
-@pytest.mark.parametrize("repetition_token", regex_repetition_tokens())
-def test_tokenizer_character_with_single_repetition_token(repetition_token):
-    assert pygex.tokenize("a" + repetition_token) == ["a" + repetition_token]
+def test_tokenizer_character_with_single_repetition_token(regex_repetition_token):
+    assert pygex.tokenize("a" + regex_repetition_token) == ["a" + regex_repetition_token]
 
-@pytest.mark.parametrize("repetition_token", regex_repetition_tokens())
-def test_tokenizer_throws_error_on_only_repetition_token(repetition_token):
-    with pytest.raises(pygex.InvalidRegexPattern) as e:
-        pygex.tokenize(repetition_token)
+def test_tokenizer_throws_error_on_only_repetition_token(regex_repetition_token):
+    with pytest.raises(pygex.InvalidRegexPattern):
+        pygex.tokenize(regex_repetition_token)
